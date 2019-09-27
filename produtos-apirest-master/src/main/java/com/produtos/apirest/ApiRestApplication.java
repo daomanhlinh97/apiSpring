2f72b562d1289b60c5eebb9e7bdde10641cdec82
daomanhlinh97 <39149996+daomanhlinh97@users.noreply.github.com>
2019-09-27 17:12:29 +0700

    Update ApiRestApplication.java

--- produtos-apirest-master/src/main/java/com/produtos/apirest/ApiRestApplication.java
+++ produtos-apirest-master/src/main/java/com/produtos/apirest/ApiRestApplication.java
@@ -2,11 +2,22 @@ package com.produtos.apirest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class ApiRestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiRestApplication.class, args);
+	}
+}
+
+
+@RestController
+class HelloController{
+	@GetMapping("/")
+	String hello() {
+		return "Hello World";
 	}
 }
