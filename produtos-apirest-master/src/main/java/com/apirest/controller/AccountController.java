272def81261b4ad637483e5e53210c6ce218180a
daomanhlinh97 <39149996+daomanhlinh97@users.noreply.github.com>
2019-09-27 20:06:07 +0700

    fix error

--- produtos-apirest-master/src/main/java/com/apirest/controller/AccountController.java
+++ produtos-apirest-master/src/main/java/com/apirest/controller/AccountController.java
@@ -9,6 +9,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,7 +22,6 @@ import org.springframework.web.bind.annotation.RestController;
 import com.apirest.dao.AccountDAO;
 import com.apirest.models.Account;
 
-@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/Account")
 public class AccountController{
