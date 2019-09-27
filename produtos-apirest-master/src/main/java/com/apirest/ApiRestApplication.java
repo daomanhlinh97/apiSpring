bd91fd5f86eefada5ada9c82a42bc714ab607161
daomanhlinh97 <39149996+daomanhlinh97@users.noreply.github.com>
2019-09-27 21:30:27 +0700

    Update ApiRestApplication.java

--- produtos-apirest-master/src/main/java/com/apirest/ApiRestApplication.java
+++ produtos-apirest-master/src/main/java/com/apirest/ApiRestApplication.java
@@ -1,9 +1,13 @@
 package com.apirest;
 
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.apirest.models.Account;
 
 @SpringBootApplication
 public class ApiRestApplication {
@@ -16,7 +20,9 @@ public class ApiRestApplication {
 @RestController
 class HelloController{
 	@GetMapping("/")
-	String hello() {
-		return "Hello World";
+	public List<Account> getAllRequest(){
+		List <Account> listReq= DAO.findAll();
+		
+		return DAO.findAll();
 	}
 }
