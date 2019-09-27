f2f6599648990fc438b0696904d82c638ff2566f
daomanhlinh97 <39149996+daomanhlinh97@users.noreply.github.com>
2019-09-27 21:17:41 +0700

    Update Account.java

--- produtos-apirest-master/src/main/java/com/apirest/models/Account.java
+++ produtos-apirest-master/src/main/java/com/apirest/models/Account.java
@@ -18,7 +18,7 @@ import org.springframework.data.annotation.LastModifiedDate;
 
 
 @Entity
-@Table(name="account")
+@Table(name="account",schema = "targetSchemaName")
 public class Account implements Serializable{
 	
 	@Id
